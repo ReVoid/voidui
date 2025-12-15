8b41f8da0865407bea6322d2fa537cf26824be1c
DPoroshin <dm.poroshin@gmail.com>
2025-12-15 18:41:50 +0300

    ITag.ts draft

--- src/components/compositions/ITag.ts
+++ src/components/compositions/ITag.ts
@@ -1,2 +1,7 @@
+type TagName = keyof HTMLElementTagNameMap;
+
 // TODO: Implement tag selection with strong types.
-export interface ITag {}
+export interface ITag<T extends TagName> {
+  tag: T;
+  props: Partial<HTMLElementTagNameMap[T]>;
+}
