4246066a2d27bd2155a6dbf67457a5e0ea6d81b0
DPoroshin <dm.poroshin@gmail.com>
2025-12-19 12:20:38 +0300

    feat: add IVoComponent interface as a flexible base for components (draft)

---
+++ src/common/IVoComponent.ts
@@ -0,0 +1,12 @@
+// TODO: Implement flexible base type for any component
+export interface IVoComponent<TModels, TProps, TEmits, TSlots> {
+  Models: TModels;
+  Props: TProps;
+  Emits: TEmits & ModelsEmits<TModels>;
+  Slots: TSlots;
+}
+
+type ModelsEmits<T> = {
+  [K in keyof T as `update:${K & string}`]: [payload: T[K]];
+};
+
