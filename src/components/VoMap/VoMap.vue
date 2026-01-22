60c79aad1a6c8fbefc5478280a0ec940cd384b07
DPoroshin <dm.poroshin@gmail.com>
2026-01-22 15:40:34 +0300

    feat: VoMap reminder

---
+++ src/components/VoMap/VoMap.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+// TODO: Implement component
+type Props = {
+  provider: 'Yandex' | 'Google';
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <iframe></iframe>
+</template>
