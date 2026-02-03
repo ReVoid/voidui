7eb19a142b7d02b24bd81c2fbdd6f0a4d5153f31
DPoroshin <dm.poroshin@gmail.com>
2026-02-03 13:41:17 +0300

    feat: add format module scaffolding

--- src/format/index.ts
+++ src/format/index.ts
@@ -1,6 +1,15 @@
+// numbers
 export * from './numeric.ts';
 export * from './currency.ts';
 export * from './percentage.ts';
-export * from './duration.ts';
+
+// dates and times
 export * from './date.ts';
 export * from './time.ts';
+export * from './duration.ts';
+
+// wording
+export * from './plural.ts';
+
+// utilities
+export * from './unbreakable.ts';
