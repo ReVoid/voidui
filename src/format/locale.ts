8823d6ece9bc8ecde966a389791f8100c2d791bb
DPoroshin <dm.poroshin@gmail.com>
2026-02-05 12:44:22 +0300

    feat: add format module scaffolding

--- src/format/locale.ts
+++ src/format/locale.ts
@@ -1,3 +1,9 @@
+
+// TODO: Implement with info https://simplelocalize.io/data/locales/ like below
+type Locale =
+  | 'ru-RU' // 🇷🇺
+  | 'en-US'; // 🇺🇸
+
 // TODO: Implement
 export function locale() {
   throw new Error('format is not implemented');
