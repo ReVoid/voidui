af56becc6e8d7150fef82c20f4e1342aece127c1
DPoroshin <dm.poroshin@gmail.com>
2026-02-11 14:31:56 +0300

    feat: VoCheckbox.vue draft

---
+++ src/components/VoCheckbox/VoCheckbox.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+// TODO: Implement component
+</script>
+
+<template>
+  <input type="checkbox" />
+</template>
---
+++ src/components/VoCheckbox/index.ts
@@ -0,0 +1,1 @@
+export { default as VoCheckbox } from './VoCheckbox.vue';
