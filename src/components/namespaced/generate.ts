c2db66f660df687d3bfa18fb51ee136cdbbeaf85
DPoroshin <dm.poroshin@gmail.com>
2026-02-20 19:07:30 +0300

    chore: todos

---
+++ src/components/namespaced/generate.ts
@@ -0,0 +1,5 @@
+// TODO: Implement namespace generation.
+// For inspiration, see: https://github.com/unovue/reka-ui/blob/v2/packages/plugins/src/namespaced/generate.ts
+export function generate() {
+  throw new Error('Not implemented');
+}
