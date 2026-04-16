c2a965415fcc21bde87b6bc5d010ff1f200ec3ab
DPoroshin <dm.poroshin@gmail.com>
2026-04-16 18:49:14 +0300

    feat: time format draft

--- src/format/time.ts
+++ src/format/time.ts
@@ -1,6 +1,29 @@
 import { defineFormat } from './defineFormat';
 
+// Millisecond amount.
+type Duration = number;
+
+// Implicit time.
+type Time = {
+  hour: number,
+  minute: number,
+  second: number,
+  millisecond: number,
+};
+
+type Value = Duration | Time;
+
+type Segment =
+  | "Hour"
+  | "Minute"
+  | "Second"
+  | "Millisecond";
+
+type Variant = "12h" | "24h";
+
+type Segments = Segment | Segment[];
+
 // TODO: Implement
-export const time = defineFormat(() => {
+export const time = defineFormat((value: Value, variant: Variant = '24h', segments: Segments = ['Hour', 'Minute']) => {
   throw new Error('format is not implemented');
 });
