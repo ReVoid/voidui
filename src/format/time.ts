050032ba584f0fc430272d45afb5a493c1382ea6
DPoroshin <dm.poroshin@gmail.com>
2026-04-16 19:10:33 +0300

    feat: time format draft

--- src/format/time.ts
+++ src/format/time.ts
@@ -1,4 +1,8 @@
+import { isNumber } from '@sniptt/guards';
+
 import { defineFormat } from './defineFormat';
+import { unbreakable } from '@/format/unbreakable.ts';
+
 
 // Millisecond amount.
 type Duration = number;
@@ -23,7 +27,42 @@ type Variant = "12h" | "24h";
 
 type Segments = Segment | Segment[];
 
-// TODO: Implement
+// TODO: Add segments.
+// TODO: Add tests.
 export const time = defineFormat((value: Value, variant: Variant = '24h', segments: Segments = ['Hour', 'Minute']) => {
-  throw new Error('format is not implemented');
+  const time: Time = (() => {
+    if(isNumber(value)) {
+      const hour = Math.floor(value / 3600000);
+      value %= 3600000;
+
+      const minute = Math.floor(value / 60000);
+      value %= 60000;
+
+      const second = Math.floor(value / 1000);
+      const millisecond = value % 1000;
+
+      return {
+        hour,
+        minute,
+        second,
+        millisecond,
+      };
+    }
+
+    return value;
+  })();
+
+  if (variant === "24h") {
+    return unbreakable(`${time.hour}:${time.minute}:${time.second}`);
+  }
+
+  if (variant === "12h") {
+    const meridian: "AM" | "PM" = time.hour >= 12
+      ? "PM"
+      : "AM";
+
+    return unbreakable(`${time.hour}:${time.minute}:${time.second} ${meridian}`);
+  }
+
+  return '';
 });
