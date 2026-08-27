10c9c5bac8b5c939bc5f5becedc88481637650af
DPoroshin <dm.poroshin@gmail.com>
2026-08-27 19:26:36 +0300

    feat: enhance ISize typing flexibility

--- src/components/compositions/ISize.ts
+++ src/components/compositions/ISize.ts
@@ -2,10 +2,17 @@ type Variant = 'XSmall' | 'Small' | 'Medium' | 'Large' | 'XLarge'
 
 type SizeUnit = number | `${number}${'px' | '%' | 'em' | 'vh' | 'vw'}` | 'auto';
 
-type SizeValue = {
+
+type _SizeValue = {
   width: SizeUnit;
   height: SizeUnit;
 };
+
+type SizeValue =
+  | SizeUnit
+  | _SizeValue
+  | Pick<_SizeValue, 'width'>
+  | Pick<_SizeValue, 'height'>;
 
 export interface ISize<T extends Variant = never> {
   size?: T | SizeValue;
